bc6c2c54b78e1612befa852ebead0c8a66ad6e3a
RateMyPsyduck <s.c.jackson@lancaster.ac.uk>
2022-07-20 22:30:07 +0100

    Hand NPC progress

---
+++ Items/Hand.cs
@@ -0,0 +1,75 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent.Bestiary;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ModLoader.Utilities;
+using Terraria.DataStructures;
+using System.Collections.Generic;   
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Enums;
+using Terraria.Audio;
+using Terraria.GameContent;
+using ReLogic.Content;
+using ReLogic.Content;
+
+namespace ATB.Items
+{
+	// Party Zombie is a pretty basic clone of a vanilla NPC. To learn how to further adapt vanilla NPC behaviors, see https://github.com/tModLoader/tModLoader/wiki/Advanced-Vanilla-Code-Adaption#example-npc-npc-clone-with-modified-projectile-hoplite
+	public class Hand : ModNPC
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Apollo's Hand");
+
+			//Main.npcFrameCount[Type] = Main.npcFrameCount[1];
+
+			NPCID.Sets.NPCBestiaryDrawModifiers value = new NPCID.Sets.NPCBestiaryDrawModifiers(0) { // Influences how the NPC looks in the Bestiary
+				Velocity = 1f // Draws the NPC in the bestiary as if its walking +1 tiles in the x direction
+			};
+			NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, value);
+		}
+
+		public override void SetDefaults() {
+			NPC.width = 64;
+			NPC.height = 45;
+			NPC.damage = 100;
+			NPC.defense = 5;
+			NPC.lifeMax = 600;
+			NPC.HitSound = SoundID.NPCHit1;
+			NPC.DeathSound = SoundID.NPCDeath2;
+			NPC.value = 60f;
+			NPC.knockBackResist = 0.5f;
+			NPC.aiStyle = 2; // Fighter AI, important to choose the aiStyle that matches the NPCID that we want to mimic
+
+			AIType = 2; // Use vanilla zombie's type when executing AI code. (This also means it will try to despawn during daytime)
+			//AnimationType = 1; // Use vanilla zombie's type when executing animation code. Important to also match Main.npcFrameCount[NPC.type] in SetStaticDefaults.
+			//SpawnModBiomes = new int[1] { ModContent.GetInstance<ExampleSurfaceBiome>().Type }; // Associates this NPC with the ExampleSurfaceBiome in Bestiary
+		}
+
+		public override float SpawnChance(NPCSpawnInfo spawnInfo) {
+			return SpawnCondition.OverworldNightMonster.Chance * 0.2f; // Spawn with 1/5th the chance of a regular zombie.
+		}
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor){
+        SpriteEffects spriteEffects = SpriteEffects.None;
+        if (NPC.spriteDirection == 1)
+            {
+                spriteEffects = SpriteEffects.FlipHorizontally;
+            }
+            return true;
+        }
+
+		private void Visuals() {
+			// So it will lean slightly towards the direction it's moving
+			NPC.rotation = NPC.velocity.X * 0.05f;	
+		}
+    }
+}
