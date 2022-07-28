3c1891f5b71fb99f35d958c273fcca5fd66da96e
RateMyPsyduck <s.c.jackson@lancaster.ac.uk>
2022-07-28 12:20:22 +0100

    Quark NPC

--- Items/ShuttleHead.cs
+++ Items/ShuttleHead.cs
@@ -1,6 +1,12 @@
 using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.Audio;
 using Terraria.DataStructures;
+using Terraria.GameContent.ObjectInteractions;
+using Terraria.ModLoader;
+using Terraria.Enums;
+using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
 
@@ -10,24 +16,100 @@ namespace ATB.Items
 	{
 		public override void SetStaticDefaults() {
 			// Properties
+			Main.tileSpelunker[Type] = true;
+			Main.tileContainer[Type] = true;
 			Main.tileFrameImportant[Type] = true;
-			Main.tileSolidTop[Type] = true;
-			Main.tileTable[Type] = true;
-
+			Main.tileNoAttach[Type] = true;
+			Main.tileOreFinderPriority[Type] = 500;
+			TileID.Sets.BasicChest[Type] = true;
+			TileID.Sets.DisableSmartCursor[Type] = true;
 			// Placement
 			TileObjectData.newTile.CopyFrom(TileObjectData.Style5x4);
 			TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16, 16 };
 			TileObjectData.newTile.StyleHorizontal = true; // Optional, if you add more placeStyles for the item 
+			TileObjectData.newTile.HookPostPlaceMyPlayer = new PlacementHook(Chest.AfterPlacement_Hook, -1, 0, false);
 			TileObjectData.addTile(Type);
+
+			AdjTiles = new int[] { TileID.Containers };
+			ContainerName.SetDefault("Shuttle");
 
 			// Etc
 			ModTranslation name = CreateMapEntryName();
-			name.SetDefault("Shuttle"); //Vanilla has no lang entry for this
 			AddMapEntry(new Color(200, 200, 200), name);
 		}
 
 		public override bool CreateDust(int i, int j, ref int type) {
 			return false;
+		}
+
+		public override bool HasSmartInteract(int i, int j, SmartInteractScanSettings settings) => true;
+		public override bool IsLockedChest(int i, int j) => false;
+
+		public override bool RightClick(int i, int j) {
+			Player player = Main.LocalPlayer;
+			Tile tile = Main.tile[i, j];
+			Main.mouseRightRelease = false;
+			int left = i;
+			int top = j;
+			if (tile.TileFrameX % 36 != 0) {
+				left--;
+			}
+
+			if (tile.TileFrameY != 0) {
+				top--;
+			}
+
+			if (player.sign >= 0) {
+				SoundEngine.PlaySound(SoundID.MenuClose);
+				player.sign = -1;
+				Main.editSign = false;
+				Main.npcChatText = "";
+			}
+
+			if (Main.editChest) {
+				SoundEngine.PlaySound(SoundID.MenuTick);
+				Main.editChest = false;
+				Main.npcChatText = "";
+			}
+
+			if (player.editedChestName) {
+				NetMessage.SendData(MessageID.SyncPlayerChest, -1, -1, NetworkText.FromLiteral(Main.chest[player.chest].name), player.chest, 1f);
+				player.editedChestName = false;
+			}
+
+			bool isLocked = IsLockedChest(left, top);
+			if (Main.netMode == NetmodeID.MultiplayerClient && !isLocked) {
+				if (left == player.chestX && top == player.chestY && player.chest >= 0) {
+					player.chest = -1;
+					Recipe.FindRecipes();
+					SoundEngine.PlaySound(SoundID.MenuClose);
+				}
+				else {
+					NetMessage.SendData(MessageID.RequestChestOpen, -1, -1, null, left, top);
+					Main.stackSplit = 600;
+				}
+			}
+			else {
+				int chest = Chest.FindChest(left, top);
+					if (chest >= 0) {
+						Main.stackSplit = 600;
+						if (chest == player.chest) {
+							player.chest = -1;
+							SoundEngine.PlaySound(SoundID.MenuClose);
+						}
+						else {
+							SoundEngine.PlaySound(player.chest < 0 ? SoundID.MenuOpen : SoundID.MenuTick);
+							player.chest = chest;
+							Main.playerInventory = true;
+							Main.recBigList = false;
+							player.chestX = left;
+							player.chestY = top;
+						}
+
+						Recipe.FindRecipes();
+					}
+				}
+			return true;
 		}
 
 	}
