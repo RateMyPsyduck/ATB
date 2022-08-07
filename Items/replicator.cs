cbcbbc2b17be62e5e804cc47a78f724314e22497
RateMyPsyduck <s.c.jackson@lancaster.ac.uk>
2022-08-07 11:08:18 +0100

    clean up

--- Items/replicator.cs
+++ Items/replicator.cs
@@ -10,26 +10,21 @@ namespace ATB.Items
 	public class replicator : ModTile
 	{
 		public override void SetStaticDefaults() {
-			// Properties
+
 			Main.tileTable[Type] = true;
 			Main.tileSolidTop[Type] = true;
 			Main.tileNoAttach[Type] = true;
 			Main.tileLavaDeath[Type] = true;
 			Main.tileFrameImportant[Type] = true;
 			TileID.Sets.DisableSmartCursor[Type] = true;
-			TileID.Sets.IgnoredByNpcStepUp[Type] = true; // This line makes NPCs not try to step up this tile during their movement. Only use this for furniture with solid tops.
+			TileID.Sets.IgnoredByNpcStepUp[Type] = true; 
 
-			// DustType = ModContent.DustType<Dusts.Sparkle>();
-			//AdjTiles = new int[] { TileID.WorkBenches };
-
-			// Placement
 			TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
 			TileObjectData.newTile.CoordinateHeights = new[] { 16, 18 };
 			TileObjectData.addTile(Type);
 
 			AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTable);
 
-			// Etc
 			ModTranslation name = CreateMapEntryName();
 			name.SetDefault("Replicator");
 			AddMapEntry(new Color(200, 200, 200), name);
