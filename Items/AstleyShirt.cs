1e4edfa5f919cea4f4ba16eca8b5b69791741549
RateMyPsyduck <s.c.jackson@lancaster.ac.uk>
2022-08-09 12:09:08 +0100

    Astlet shirt progress

---
+++ Items/AstleyShirt.cs
@@ -0,0 +1,26 @@
+﻿using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ATB.Items
+{
+	// The AutoloadEquip attribute automatically attaches an equip texture to this item.
+	// Providing the EquipType.Body value here will result in TML expecting X_Arms.png, X_Body.png and X_FemaleBody.png sprite-sheet files to be placed next to the item's main texture.
+	[AutoloadEquip(EquipType.Body)]
+	public class AstleyShirt : ModItem
+	{
+		public override void SetStaticDefaults() {
+			base.SetStaticDefaults();
+			DisplayName.SetDefault("Astley Shirt");
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults() {
+			Item.width = 26; // Width of the item
+			Item.height = 18; // Height of the item
+			Item.rare = ItemRarityID.Green; // The rarity of the item
+			Item.defense = 6; // The amount of defense the item will give when equipped
+		}
+	}
+}
